1357eed15a1fada320a1438545024ca22e66e0d9
Yong Hyun <pyh4616@naver.com>
2023-06-26 20:04:44 +0900

    설정 완료

---
+++ src/main/java/com/yonghyeon/ingram/IngramApplication.java
@@ -0,0 +1,13 @@
+package com.yonghyeon.ingram;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IngramApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IngramApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/yonghyeon/ingram/ViewControllerTest.java
@@ -0,0 +1,61 @@
+package com.yonghyeon.ingram;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class ViewControllerTest {
+
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+	public String home() {
+		return "index";
+	}
+
+	@GetMapping("/auth/signup")
+	public String signupPage() {
+		return "auth/signup";
+	}
+	
+	@GetMapping("/auth/signin")
+	public String signinPage() {
+		return "auth/signin";
+	}
+	
+	@GetMapping("/image/story")
+	public String storyPage() {
+		return "image/story";
+	}
+	
+	@GetMapping("/image/popular")
+	public String popularPage() {
+		return "image/popular";
+	}
+	
+	@GetMapping("/image/upload")
+	public String uploadPage() {
+		return "image/upload";
+	}
+	
+	@GetMapping("/user/profile")
+	public String profilePage() {
+		return "user/profile";
+	}
+
+	@GetMapping("/user/update")
+	public String updatePage() {
+		return "user/update";
+	}
+
+	@GetMapping("/ex")
+	public String exampleQuery(String type) {
+		return type;
+	}
+
+	@GetMapping("/ex/{type}")
+	public String examplePath(@PathVariable String type) {
+		return type;
+	}
+}
